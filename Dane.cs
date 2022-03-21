74bfb92d64ec45d89f64e2bce37cfe705eda44f3
PMBytner <pawel.m.bytner@gmail.com>
2022-03-21 09:33:35 +0100

    Dodaj pliki projektów.

---
+++ Dane.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_INIS4_PR2._2_z3
+{
+    class Dane
+    {
+        public string KolorObramowania { get; set; } = "Black";
+        public string KolorTła { get; set; } = "#888888";
+        public string KolorTekstu { get; set; } = "#FF0099";
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NET_INIS4_PR2._2_z3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dane dane = new Dane();
+        public MainWindow()
+        {
+            DataContext = dane;
+            InitializeComponent();
+        }
+    }
+}
