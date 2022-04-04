7e98251eade531c3de94bdf43eddf1b4f862f49a
PMBytner <pawel.m.bytner@gmail.com>
2022-04-04 09:00:33 +0200

    datagrid z użyciem kolekcji c# i samodzielnie zdefiniowanej klasy

--- Dane.cs
+++ Dane.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace NET_INIS4_PR2._2_z3
 {
+    class Osoba
+    {
+        static uint następneID = 0;
+        string
+            imię,
+            nazwisko
+            ;
+
+        public string Imię { get => imię; set => imię = value; }
+        public string Nazwisko { get => nazwisko; set => nazwisko = value; }
+        public string ImięNazwisko { get => $"{imię} {nazwisko}"; }
+        public uint Wiek { get; set; } = 0;
+        public uint ID { get; } = następneID++;
+    }
     class Dane
     {
-        public string KolorObramowania { get; set; } = "Black";
-        public string KolorTła { get; set; } = "#888888";
-        public string KolorTekstu { get; set; } = "#FF0099";
+        public Osoba[] Osoby { get; set; } = {
+            new Osoba() {Imię="Adam",Nazwisko="Wiśniewski",Wiek=30},
+            new Osoba() {Imię="Marian",Nazwisko="Dąbrowski",Wiek=29},
+            new Osoba() {Imię="Julia",Nazwisko="Jabłońska",Wiek=15},
+            new Osoba() {Imię="Sylwia",Nazwisko="Sosnowska",Wiek=25},
+        };
     }
 }
